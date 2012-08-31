a73c46724d31aedb53945f33f07cb6775e9880e5
Gabriel Rubens <grsabreu@gmail.com>
2012-08-31 20:46:18 -0300

    Optimizations

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Leran
 ### A Ruby file organizator
 
 Try in your terminal "leran.rb help" for details
+
+If you are a Linux user, at the first use run this command on terminal
+
+chmod +x leran
--- leran.rb
+++ leran.rb
@@ -1,34 +1,39 @@
 #!/usr/bin/env ruby
 # Bill Hotter/I goes/go to learn!
-require "fileutils"
+require 'fileutils'
+
 class Leran
 	@@fileTypes = {
-			'Video'		=> 
+			'Video': 
 		%w(.AVI .MKV .MP4 .MPEG .MPG .VOB .RMVB .WMV .FLV .MOV),
 
-			'Music'		=> 
+			'Music':
 		%w(.MP3 .AMR .WAV .WMA .ACC .OGG),
 
-			'Img'		=> 
+			'Img':
 		%w(.JPG .JPEG .BMP .PNG .GIF .ICO .PSD .XCF),
 
-			'Docs'		=>
+			'Docs':
 		%w(.ODT .DOC .DOCX .XLS .XLSX .PPSX .PPS .PDF),
 
-			'Others'	=> 
-		%w(.TXT .RAR .ZIP .TAR .TAR.GZ .TAR.BZ2 .EXE .TORRENT)
+			'Others':
+		%w(.TXT .RAR .ZIP .TAR .EXE .TORRENT)
+		
 	}
 	
 	@@types = @@fileTypes.keys
+	
 	# Firt step: Get the 'from'/'to' folders
 	# def scanFolder: Find folders and files
 	# def moveFile: do... duhhhh
 	
 	def initialize( fromDir, toDir, isDeep )
 		@qtMoveds = 0
-		toDir = fromDir if toDir == "-e"
-		puts "_" * 50 
-		puts "Entering the store directory and creating folders"
+		toDir = fromDir if toDir == '-e'
+		
+		puts '_' * 50 
+		puts 'Entering in the store directory and creating folders'
+		
 		scanFolder fromDir,toDir,isDeep
 	end
 	
@@ -36,9 +41,10 @@ class Leran
 		Dir.foreach( from ) do |file|
 			if !File.directory? "#{from}/#{file}" 
 				ext = File.extname( file ).upcase
+				
 				moveFile from,to,file,ext if ext != ""
 			elsif deep == '-d' || !file =~ /^\.{1,2}/ # ignore dirs '.' and '..'
-				scanFolder "#{from}/#{file}",to,'-d'  if !(@@types.include? file)
+				scanFolder( "#{from}/#{file}", to, '-d' ) if !(@@types.include? file)
 			end		
 		end
 	end
@@ -49,21 +55,25 @@ class Leran
 				puts "Moving: #{file.slice(0,20)}... | Type: #{type}" 
 				
 				# First: Create the folder for type
-				# Second: Create the folder for extension
-				# Three: Move the files				
+				# Second: Create the folder for extension			
 				
-				Dir.mkdir "#{to}/#{type}",0755 if !File::exists? "#{to}/#{type}"
-				Dir.mkdir "#{to}/#{type}/#{ext}",0755 if !File::exists? "#{to}/#{type}/#{ext}"
+				Dir.mkdir( "#{to}/#{type}", 0755 ) if !File::exists? "#{to}/#{type}"
+				Dir.mkdir( "#{to}/#{type}/#{ext}", 0755 ) if !File::exists? "#{to}/#{type}/#{ext}"
+				
 				FileUtils.mv "#{from}/#{file}", "#{to}/#{type}/#{ext}"
 				@qtMoveds += 1
 			end			
 		end
 	end
+	
 	def totalMoveds
 		@qtMoveds
 	end
+	
 end
+
 # Initialize it!
+
 if ARGV[0] == "help" || ARGV[0] == nil
 	puts "Use demonstration: 
 		leran.rb fromDir toDir [isDeep]
