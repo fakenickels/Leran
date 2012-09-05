ec3449e3982293ae5f2f89131482c1957a332d3e
Gabriel Rubens <grsabreu@gmail.com>
2012-09-05 08:50:54 -0300

    Any fixes...

--- leran.rb
+++ leran.rb
@@ -20,6 +20,7 @@ class Leran
 	}
 
 	@@types = @@fileTypes.keys
+	
 	# Firt step: Get the 'from'/'to' folders
 	# def scanFolder: Find folders and files
 	# def moveFile: do... duhhhh
@@ -52,18 +53,22 @@ class Leran
 				# Second: Create the folder for extension
 				# Three: Move the files				
 				
-				Dir.mkdir "#{to}/#{type}",755 if !File::exists? "#{to}/#{type}"
-				Dir.mkdir "#{to}/#{type}/#{ext}",755 if !File::exists? "#{to}/#{type}/#{ext}"
+				Dir.mkdir "#{to}/#{type}",0755 if !File::exists? "#{to}/#{type}"
+				Dir.mkdir "#{to}/#{type}/#{ext}",0755 if !File::exists? "#{to}/#{type}/#{ext}"
 				FileUtils.mv "#{from}/#{file}", "#{to}/#{type}/#{ext}"
 				@qtMoveds += 1
 			end			
 		end
 	end
+	
 	def totalMoveds
 		@qtMoveds
 	end
+	
 end
+
 # Initialize it!
+
 if ARGV[0] == "help" || ARGV[0] == nil
 	puts "Use demonstration: 
 		leran.rb fromDir toDir [isDeep]
